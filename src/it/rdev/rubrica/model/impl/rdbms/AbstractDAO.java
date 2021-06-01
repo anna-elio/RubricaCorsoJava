8849b175214477b7ed70213d8914abc5820d388e
Danilo Di Nuzzo <danilo.dinuzzo@r-dev.it>
2021-06-01 15:54:08 +0200

    Fix visibilità classi DAO

--- src/it/rdev/rubrica/model/impl/rdbms/AbstractDAO.java
+++ src/it/rdev/rubrica/model/impl/rdbms/AbstractDAO.java
@@ -10,14 +10,14 @@ import it.rdev.rubrica.model.DAO;
 
 /**
  * 
- * Classe astratta che contiene la logica comune per le operazioni di CRUD
+ * Classe astratta che contiene la logica comune per le operazioni di CRUD su database relazionale
  * 
  * @author Danilo Di Nuzzo
  *
  * @param <T> entità su cui effettuare le operazioni di CRUD
  * @param <D> tipo di cui è formato l'ID dell'entità da gestire (tipo del database)
  */
-public abstract class AbstractDAO<T, D> implements DAO<T> {
+abstract class AbstractDAO<T, D> implements DAO<T> {
 	
 	/**
 	 * Metodo utilizzato per l'esecuzione di queries che non richiedono parametri di input
